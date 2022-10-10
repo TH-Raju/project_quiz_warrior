f88355621299fdbbe106fb06e53cf08f5d3a67c3
TH-Raju <rajukhan894200@gmail.com>
2022-10-11 03:43:17 +0600

    Quiz Card added

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import QuizCard from './QuizCard';
 
 const Topics = () => {
+    const quizes = useLoaderData().data;
     return (
-        <div>
-            <h1>Topics</h1>
+        <div className="grid grid-cols-1 gap-x-4 gap-y-8  justify-items-center md:grid-cols-2 lg:grid-cols-4">
+            {
+                quizes.map(quiz => <QuizCard
+                    key={quiz.id}
+                    quiz={quiz}
+                ></QuizCard>)
+            }
         </div>
     );
 };
